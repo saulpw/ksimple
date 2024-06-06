e6391979938b23cb385f96a534bf4dc9dc7f83ee
Saul Pwanson <code@saul.pw>
2024-06-05 19:11:23 -0700

    rename g() to global()

--- a.c
+++ a.c
@@ -118,14 +118,14 @@ char*adverbs=" /\\";
 u(*adverbs2[])(u,u)={0,Ovr,Scn};                    //!< adverbs[]/adverbs2[] is the same as verbs[]/verbs2[], only for adverbs.
 
 //!globals, verbs, nouns, adverbs
-def1(g,x>='a'&&x<='z')                              //!< is x a valid (g)lobal variable identifier?
+def1(global,x>='a'&&x<='z')                         //!< is x a valid (g)lobal variable identifier?
 def2(ag,y(U[f],!ay?unalloc(y):x;incref(U[f]=x)))    //!< (a)ssign (g)lobal: release no longer referenced global object at U[f], and replace it with object x.
 def1(verb,(strchr(verbs,x)?:verbs)-verbs)           //!< is x a valid verb from verbs? if so, return its index, otherwise return 0.
                                                     //!< \note rarely seen ternary form x?:y, which is just a shortcut for x?x:y in c.
 def1(adverb,(strchr(adverbs,x)?:adverbs)-adverbs)   //!< same as verb() for adverbs.
 def1(noun,10>x-'0'                                  //!< is x a (n)oun? valid nouns are digits 0..9 and lowercase varnames a..z.
            ?x-'0'                                   //!< if x is a digit, e.g. '7', return its decimal value.
-           :g(x)?incref(U[x-'a'])                   //!< if x is a varname, e.g. 'a', return its value from U[26] and increment its refcount.
+           :global(x)?incref(U[x-'a'])              //!< if x is a varname, e.g. 'a', return its value from U[26] and increment its refcount.
                 :ERR)                               //!< ..anything else is an error.
 
 //!fio
@@ -150,7 +150,7 @@ defstr(eval,                                        //!< (e)val: recursively eva
            :y(                                      //!< in case if i is not a verb, it must be a valid noun, and the next token after a noun should be a verb,
               eval(t+1),Q(y)                        //!<   recursively evaluate next token to the right of the verb and put result into y. bail out on error.
               ':'==*t                               //!<   special case: if y is preceded by a colon instead of a verb, it is an inline assignment (eg 1+a:1),
-                    ?x(g(i),Qp()ag(i-'a',y))        //!<   so i should be a (g)lobal varname a..z. if so, increment y's refcount, store it in U[26], and return it.
+                    ?x(global(i),Qp()ag(i-'a',y))   //!<   so i should be a (g)lobal varname a..z. if so, increment y's refcount, store it in U[26], and return it.
                     :x(noun(i),Qp()                 //!<   x is a noun to the left of the verb. throw parse error if it is invalid.
                          u8 f=verb(*t);Qd(!f)       //!<   f is the index of the verb to the left of noun y. if it's not a valid verb, throw domain error.
                          verbs2[f](x,y))))          //!< apply dyadic verb f to nouns x and y (e.g. 2+3) and return result (noun or error).
